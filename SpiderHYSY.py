8b990a085aecc4deda9e13a322707c1a4b57dfbf
weifei <hanfei@firewinggames.com>
2016-12-20 16:18:08 +0800

    post ok ,can find the book you want

--- SpiderHYSY.py
+++ SpiderHYSY.py
@@ -11,8 +11,9 @@ url = 'http://www.huanyue123.com/modules/article/search.php'
 
 # user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 
-strs = '圣墟'
-values = {'searchtype': 'articlename', 'searchkey': strs}
+strs = u'圣墟'  # 一定要有这个 u  没有u的话请求的编码会有错误
+
+values = {'searchkey': strs}
 
 data = urllib.urlencode(values)
 
@@ -21,9 +22,11 @@ try:
     request = urllib2.Request(url, data=data)
     response = urllib2.urlopen(request)
     content = response.read().decode('gbk')
+    head = response.info()
     f = file('hy.html', 'w')
     f.write(content)
     f.close()
+    print head
     print content
 
 except urllib2.URLError, e:
